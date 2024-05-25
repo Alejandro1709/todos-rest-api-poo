d8546368eed001a5648c8caf228b866b7b6ee25d
= <alejandro.lr.developer@hotmail.com>
2024-05-25 11:36:32 -0500

    Add: Root endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
+import { Request, Response } from "express";
 import app from "./app";
 import { PORT } from "./config/secrets";
+
+app.get("/", (_req: Request, res: Response) => {
+  res.status(200).json({ message: "Ok" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port: ${PORT}`);
